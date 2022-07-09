769488a1e382bdcbf5cc92f99e52e614ee1073b3
Charly Delay <0xcharly@users.noreply.github.com>
2022-07-09 13:47:17 +0900

    Add missing files

---
+++ .config/nvim/after/plugin/comment.lua
@@ -0,0 +1,80 @@
+if not pcall(require, 'Comment') then return end
+
+require('Comment').setup {
+  -- LHS of operator-pending mapping in NORMAL + VISUAL mode
+  opleader = {
+    -- line-comment keymap
+    line = '<Leader>gc',
+    -- block-comment keymap
+    block = '<Leader>gb',
+  },
+
+  -- Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
+  mappings = {
+
+    -- operator-pending mapping
+    -- Includes:
+    --  `gcc`               -> line-comment  the current line
+    --  `gcb`               -> block-comment the current line
+    --  `gc[count]{motion}` -> line-comment  the region contained in {motion}
+    --  `gb[count]{motion}` -> block-comment the region contained in {motion}
+    basic = true,
+
+    -- extra mapping
+    -- Includes `gco`, `gcO`, `gcA`
+    extra = true,
+
+    -- extended mapping
+    -- Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
+    extended = true,
+  },
+
+  -- LHS of toggle mapping in NORMAL + VISUAL mode
+  toggler = {
+    -- line-comment keymap
+    --  Makes sense to be related to your opleader.line
+    line = '<Leader>gcc',
+
+    -- block-comment keymap
+    --  Make sense to be related to your opleader.block
+    block = '<Leader>gbc',
+  },
+
+  -- Pre-hook, called before commenting the line
+  --    Can be used to determine the commentstring value
+  ---@param ctx CommentCtx
+  pre_hook = function(ctx)
+    -- Only calculate commentstring for tsx filetypes
+    if vim.bo.filetype == 'typescriptreact' then
+      local U = require('Comment.utils')
+
+      -- Determine whether to use linewise or blockwise commentstring
+      local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
+
+      -- Determine the location where to calculate commentstring from
+      local location = nil
+      if ctx.ctype == U.ctype.block then
+        location =
+            require('ts_context_commentstring.utils').get_cursor_location()
+      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+        location =
+            require('ts_context_commentstring.utils').get_visual_start_location()
+      end
+
+      return
+          require('ts_context_commentstring.internal').calculate_commentstring({
+            key = type,
+            location = location,
+          })
+    end
+  end,
+
+  -- Post-hook, called after commenting is done
+  --    Can be used to alter any formatting / newlines / etc. after commenting
+  post_hook = nil,
+
+  -- Can be used to ignore certain lines when doing linewise motions.
+  --    Can be string (lua regex)
+  --    Or function (that returns lua regex)
+  ignore = nil,
+}
