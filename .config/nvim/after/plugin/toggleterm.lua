b2c0ab0bb7840cf6f19d2df3f5f70dcf327aa412
Charly Delay <0xcharly@users.noreply.github.com>
2022-08-19 20:50:15 +0900

    Add missing files

---
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,18 @@
+-- Terminal config.
+vim.o.shell = "/opt/homebrew/bin/fish"
+
+require("toggleterm").setup()
+
+vim.keymap.set("n", "<C-M-t>", ":ToggleTerm direction=float<CR>")
+vim.keymap.set("t", "<C-M-t>", "<C-\\><C-N>:ToggleTerm direction=float<CR>")
+
+require("which-key").register({
+  t = {
+    name = "+terminal",
+    h = { ":ToggleTerm direction=horizontal<CR>", "[H]orizontal" },
+    v = { ":ToggleTerm direction=vertical<CR>", "[V]ertical" },
+    t = { ":ToggleTerm direction=tab<CR>", "[T]ab" },
+    f = { ":ToggleTerm direction=float<CR>", "[F]loat" },
+    ["<space>"] = { ":ToggleTerm<CR>", "[T]oggle" },
+  },
+}, { prefix = "<leader>" })
